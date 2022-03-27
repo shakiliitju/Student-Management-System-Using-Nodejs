7f12fc16e9c165df9a172d1b2568189cb32fabe6
Md. Shakil Hossain <shakiljusc15@gmail.com>
2022-03-27 21:06:45 +0600

    update

--- server/routes/user.js
+++ server/routes/user.js
@@ -6,11 +6,15 @@ const userController = require('../controllers/userController');
 
 router.get('/', userController.view);
 router.post('/', userController.find);
+router.get('/result', userController.views);
 router.get('/adduser', userController.form);
 router.post('/adduser', userController.create);
+router.get('/addresult', userController.forms);
+router.post('/addresult', userController.creates);
 router.get('/edituser/:id', userController.edit);
 router.post('/edituser/:id', userController.update);
 router.get('/viewuser/:id', userController.viewall);
+router.get('/viewresult/:id', userController.viewalls);
 router.get('/:id',userController.delete);
   
 module.exports = router;
